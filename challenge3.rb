040104ee7ab959aab3c60981124050bb210e1b01
Justin Lee <justinslee201100@yahoo.com>
2023-08-08 10:30:46 -0600

    Answer challenge3

---
+++ challenge3.rb
@@ -0,0 +1,15 @@
+1. Write a method or function that removes all instances of the letter <strong>s</strong> in a string.
+ The method or function should accept a string as an argument and return the same string with 
+ every instance of the letter <strong>s</strong> removed.
+
+ #make a method that exterminates a letter upper and lowercase. method needs just one string and take all
+ #the s's out of that string
+
+ #make method not confuzing name and put variable in there
+ #giv method directions to get rid of the letter s and S
+ #print out the method with the sting in as the variable 
+ 
+ def pizza(name)
+    name.delete("s") && name.delete("S")
+ end
+ puts pizza("paSS")
